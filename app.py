32f5143c5c7a8317ae6337b4849d555d98fc87c3
Nilmar Publio de Castro <nilmarpublio@gmail.com>
2025-08-27 18:53:34 -0300

    Corrigir app.py com conteúdo correto

---
+++ app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Arquivo de redirecionamento para app_postgres.py
+"""
+
+# Importar e executar app_postgres.py
+from app_postgres import app
+
+if __name__ == '__main__':
+    app.run()
